cc050f6c9cb8e5fb38c728cf0d7a7d45cd79930d
garam <garam-park@naver.com>
2018-10-18 00:30:27 +0900

    #1 인증 부분 없이 테스트 구현

--- tests/Feature/SchoolsTest.php
+++ tests/Feature/SchoolsTest.php
@@ -7,5 +7,24 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SchoolsTest extends TestCase
 {
-    
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCreateTest()
+    {
+        //TODO 로그인/인증 사용자만
+        $response = $this->json('POST', '/api/schools', [
+            'name' => 'Sally',
+            'location' => 'Sally',
+        ]);
+        
+        
+        $response->assertStatus(201)
+        ->assertJsonFragment([
+            'name' => 'Sally',
+            'location' => 'Sally',
+        ]);
+    }
 }
