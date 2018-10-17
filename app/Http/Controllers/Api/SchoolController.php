52f647de0d02a0fa3f58b7b6458f2be6fb35c549
garam <garam-park@naver.com>
2018-10-18 00:28:50 +0900

    #1 생성하도록 처리, 인증 부분 해야함

--- app/Http/Controllers/Api/SchoolController.php
+++ app/Http/Controllers/Api/SchoolController.php
@@ -4,8 +4,46 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
+
+use App\Models\School;
 
 class SchoolController extends Controller
 {
-    
+    public function create(Request $request)
+    {
+
+        $school_dto = $request->only([
+            'name',
+            'location'
+        ]);
+
+        $validator = Validator::make($school_dto, [
+            'name'     => 'required|string|max:191',
+            'location' => 'required|string|max:191',
+        ]);
+
+        if ($validator->fails()) {
+            
+            $errors = $validator->errors()->all();
+            
+            return response([
+                'message' => "validation failed",
+                'errors'  => $errors
+            ],400);
+        }
+
+        try {
+            $school = School::create($school_dto);
+            //TODO 생성 요청 사용자가 관리자가 된다.
+            
+            return response($school,201);
+        }catch(\Exception $e){
+            return response([
+                'message' => "validation failed",
+                'errors'  => [$e->getMessage()]
+            ],400);
+        }
+        
+    }
 }
