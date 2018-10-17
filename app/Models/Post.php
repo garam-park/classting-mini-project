34698d0bbe1f45fc92d5ef4eae7fc8990807e4d4
garam <garam-park@naver.com>
2018-10-17 20:44:19 +0900

    모델 클래스 정의

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,52 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class User
+ *
+ * @package App\Models
+ *
+ * @author  Garam Park <garam-park@naver.com>
+ *
+ * @OA\Schema(
+ *     title="Post model",
+ *     description="Post model",
+ * )
+ */
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+    /**
+     * @OA\Property(
+     *     format="int64",
+     *     description="ID",
+     *     title="ID",
+     * )
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     * @OA\Property(
+     *     description="Title",
+     *     title="Title",
+     * )
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @OA\Property(
+     *     description="Content",
+     *     title="Content",
+     * )
+     *
+     * @var string
+     */
+    public $content;
 }
--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -4,7 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class User
+ *
+ * @package App\Models
+ *
+ * @author  Garam Park <garam-park@naver.com>
+ *
+ * @OA\Schema(
+ *     title="Subscription model",
+ *     description="Subscription model",
+ * )
+ */
 class Subscription extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'school_id',
+    ];
+    
+    
+    /**
+     * @OA\Property(
+     *     format="int64",
+     *     description="school_id",
+     *     title="school_id",
+     * )
+     *
+     * @var integer
+     */
+    public $school_id;
+
+    /**
+     * @OA\Property(
+     *     format="int64",
+     *     description="user_id",
+     *     title="user_id",
+     * )
+     *
+     * @var integer
+     */
+    public $user_id;
+
 }
