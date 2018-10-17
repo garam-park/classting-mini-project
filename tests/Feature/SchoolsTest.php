696a3b33d128b625bb8da49246e7dc14b4cfdb9c
garam <garam-park@naver.com>
2018-10-18 00:31:47 +0900

    #1 인증 부분 완료 후에 테스트를 더 추가해야한다.

--- tests/Feature/SchoolsTest.php
+++ tests/Feature/SchoolsTest.php
@@ -14,7 +14,13 @@ class SchoolsTest extends TestCase
      */
     public function testCreateTest()
     {
-        //TODO 로그인/인증 사용자만
+        /**
+         * //TODO 로그인/인증 사용자만
+         * #1 인증 부분 완료 후에 테스트를 더 추가해야한다.
+         */
+        
+        $this->assertTrue(false);
+
         $response = $this->json('POST', '/api/schools', [
             'name' => 'Sally',
             'location' => 'Sally',
